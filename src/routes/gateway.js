a0054e868d7408e3eb2ab5e5da08fbefb95e802d
Vinícius Trainotti <vtrainotti@live.com>
2022-05-18 21:05:21 -0300

    [TESTES] Add healthz

--- src/routes/gateway.js
+++ src/routes/gateway.js
@@ -3,5 +3,6 @@ const router  = express.Router();
 const userRoutes = require('./user');
 
 router.use("/user", userRoutes);
+router.use("/healthz", (req, res, next) => {  return res.status(200).send("OK"); })
 
 module.exports = router;
